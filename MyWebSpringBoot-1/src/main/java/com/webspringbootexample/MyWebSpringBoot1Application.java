aabf3e2453bed1b228de9f7a78437862ed6a281d
test <test@DESKTOP-I19V3OI>
2019-08-03 13:56:16 +0530

    fsdf

--- MyWebSpringBoot-1/src/main/java/com/webspringbootexample/MyWebSpringBoot1Application.java
+++ MyWebSpringBoot-1/src/main/java/com/webspringbootexample/MyWebSpringBoot1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyWebSpringBoot1Application {
+  public class MyWebSpringBoot1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebSpringBoot1Application.class, args);
